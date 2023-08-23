4ec22f3c155a0649d3adee8f430decd40bf7cff5
LAGNER <egorlagner@gmail.com>
2023-08-23 15:51:36 +0300

    добавлен функция _parsing_members_by_lists сделать код более DRY

---
+++ sandbox/data.py
@@ -0,0 +1,28 @@
+class Car:
+    # Атрибуты класса
+    make = ""
+    model = ""
+    year = 0
+    color = ""
+
+    # Конструктор класса (специальный метод __init__)
+    def __init__(self, make='mazda', model='6', year='2011', color='gray'):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    # Метод для вывода информации о машине
+    def display_info(self):
+        print(f"Марка: {self.make}")
+        print(f"Модель: {self.model}")
+        print(f"Год выпуска: {self.year}")
+        print(f"Цвет: {self.color}")
+
+    # Метод для запуска двигателя
+    def start_engine(self):
+        print(f"{self.make} {self.model}: Двигатель запущен")
+
+    # Метод для остановки двигателя
+    def stop_engine(self):
+        print(f"{self.make} {self.model}: Двигатель остановлен")
--- utils.py
+++ utils.py
@@ -1,19 +1,26 @@
+def _parsing_members_by_lists(obj):
+    all_members = dir(obj)  # Получить все члены класса. Члены - общее название для атрибутов и методов
+
+    obj_methods = []
+    obj_attributes = []
+
+    for member in all_members:
+        member_obj = getattr(obj, member)  # Поверяет, является ли член атрибутом
+        if callable(member_obj):  # Проверяем, является ли член вызываемым объектом. Атрибуты ими не являются
+            obj_methods.append(member)
+        else:
+            obj_attributes.append(member)
+
+    return obj_attributes, obj_methods
+
+
 class ToolGetAllPropertyObj:
     """Разбирает атрибуты и методы объекта по спискам"""
 
     def __init__(self, obj):
         self.obj = obj
-        self.obj_attributes = []
-        self.obj_methods = []
 
-        all_members = dir(obj)  # Получить все члены класса. Члены - общее название для атрибутов и методов
-
-        for member in all_members:
-            member_obj = getattr(obj, member)  # Поверяет, является ли член атрибутом
-            if callable(member_obj):  # Проверяем, является ли член вызываемым объектом. Атрибуты ими не являются
-                self.obj_methods.append(member)
-            else:
-                self.obj_attributes.append(member)
+        self.obj_attributes, self.obj_methods = _parsing_members_by_lists(obj)
 
     def print_attributes(self):
         print(f'аттрибуты объекта {self.obj}')
