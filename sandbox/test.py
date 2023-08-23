2844b809c99287e2f9334c3577c56936028163da
LAGNER <egorlagner@gmail.com>
2023-08-23 16:07:16 +0300

    добавлены методы print_attributes, print_methods для класса ToolGetAllPropertyObj
    Выводят атрибуты и методы без магических методов

---
+++ sandbox/test.py
@@ -0,0 +1,10 @@
+from data import Car
+from utils import ToolGetAllPropertyObj
+
+
+obj = ToolGetAllPropertyObj(Car)
+obj.print_all_attributes()
+obj.print_attributes()
+obj.print_all_methods()
+obj.print_methods()
+
--- utils.py
+++ utils.py
@@ -22,16 +22,32 @@ class ToolGetAllPropertyObj:
 
         self.obj_attributes, self.obj_methods = _parsing_members_by_lists(obj)
 
-    def print_attributes(self):
+    def print_all_attributes(self):
         print(f'аттрибуты объекта {self.obj}')
         for attr in self.obj_attributes:
             print(attr)
         print()
 
-    def print_methods(self):
+    def print_all_methods(self):
         print(f'методы объекта {self.obj}')
         for method in self.obj_methods:
             print(method)
+        print()
+
+    def print_attributes(self):
+        print(f'аттрибуты объекта {self.obj}')
+        for attr in self.obj_attributes:
+            if attr.startswith('__'):
+                continue
+            print(attr)
+        print()
+
+    def print_methods(self):
+        print(f'аттрибуты объекта {self.obj}')
+        for attr in self.obj_attributes:
+            if attr.startswith('__'):
+                continue
+            print(attr)
         print()
 
 
